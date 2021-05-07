55838dc96f83bfd1021dc8d23632fcddfc6be2df
lxfu1 <954055752@qq.com>
2021-05-07 10:09:59 +0800

    fix: recover website

--- src/index.g6.ts
+++
@@ -1,4 +0,0 @@
-// G6
-import { OrganizationTreeGraph, DagreGraph, IndentedTree, DagreFundFlowGraph } from './graph';
-
-export { OrganizationTreeGraph, DagreGraph, IndentedTree, DagreFundFlowGraph };
